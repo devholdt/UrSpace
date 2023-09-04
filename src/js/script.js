7fc7a602b03fa77e27f205c2e62e959721ed33ab
MHerholdt94 <mathiasher@live.no>
2023-09-04 18:26:19 +0200

    JSDoc commit 1
    - Added script to prepare for using JSDoc at a later time
    - Created script.js file (with JSDoc lesson task)
    - Added script.js CDN to HTML file
    - Added conf.json file to make sure node_modules is excluded when creating JSDocs

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @param {number} a This is the first value
+ * @param {number} b This is the second value
+ * @returns {number} The sum of value a and value b
+ * @example
+ * // Add a number value to params a and b
+ * ```js
+ * addNumbers(4, 7);
+ * ```
+ * // Returns number 11
+ */
+function addNumbers(a, b) {
+  const result = a + b;
+  return result;
+}
