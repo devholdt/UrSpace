1a5b90c5a1204faf0e47bf73ada791495ddb52f5
MHerholdt94 <mathiasher@live.no>
2023-09-19 13:33:40 +0200

    User registration and login
    Added JS for user registration and login with calls to the provided API.
    
    Minor changes to variable names, placeholder texts, HTML element IDs and some

--- src/js/login.js
+++ src/js/login.js
@@ -1,49 +1,75 @@
 import renderMenu from "./components/renderMenu.js";
 import { formValidation } from "./utilities/formValidation.js";
-// import message from "./components/message.js";
+import { API_BASE_URL } from "./settings/api.js";
+import { storageSetItem } from "./utilities/storage.js";
 
 renderMenu();
 formValidation();
 
-// const form = document.querySelector("form");
-// const email = document.querySelector("#email");
-// const password = document.querySelector("#password");
-// const messageContainer = document.querySelector(".message-container");
+const form = document.querySelector("form");
+const emailInput = document.getElementById("loginEmail");
+const passwordInput = document.getElementById("loginPassword");
 
-// form.addEventListener("submit", submitForm);
+form.addEventListener("submit", handleLogin);
 
-// function submitForm(event) {
-//   event.preventDefault();
+/**
+ * Handles the submission of a user login form.
+ *
+ * This function prevents the default form submission behavior, collects
+ * user input values, sends a login request to the server, and
+ * handles the response.
+ *
+ * @param {Event} event The event object representing the form submission.
+ * @returns {Promise<void>} A Promise that resolves when the login is complete.
+ */
+async function handleLogin(event) {
+  event.preventDefault();
 
-//   messageContainer.innerHTML = "";
+  const loginUrl = `${API_BASE_URL}/social/auth/login`;
 
-//   const emailValue = email.value.trim();
-//   const passwordValue = password.value.trim();
+  const email = emailInput.value;
+  const password = passwordInput.value;
 
-//   // If values are invalid/absent, display an error message
-//   if (emailValue.length === 0 && passwordValue.length === 0) {
-//     return message(
-//       "bg-warning",
-//       "Please enter your login details",
-//       ".message-container"
-//     );
-//   } else if (emailValue.length === 0) {
-//     return message(
-//       "bg-warning",
-//       "Please enter your e-mail",
-//       ".message-container"
-//     );
-//   } else if (passwordValue.length === 0) {
-//     return message(
-//       "bg-warning",
-//       "Please enter a password",
-//       ".message-container"
-//     );
-//   }
+  if (email.length === 0 || password.length === 0) {
+    return;
+  } else {
+    const user = {
+      email: email,
+      password: password,
+    };
 
-//   doLogin(emailValue, passwordValue);
-// }
+    try {
+      const postData = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      };
 
-// async function doLogin(email, password) {
-//   console.log("Hello");
-// }
+      const response = await fetch(loginUrl, postData);
+      console.log(response);
+
+      if (response.ok) {
+        const json = await response.json();
+        console.log(json);
+
+        const token = json.accessToken;
+        storageSetItem("token", token);
+
+        // Success message here
+      } else {
+        // Error message here
+
+        console.log("An error has occured when attempting the user login");
+      }
+    } catch (error) {
+      console.log(error);
+      // Error message here
+    }
+  }
+}
+
+// mathi_test
+// mathi_test@noroff.no
+// password123bad
--- src/js/signup.js
+++ src/js/signup.js
@@ -1,5 +1,77 @@
 import renderMenu from "./components/renderMenu.js";
 import { formValidation } from "./utilities/formValidation.js";
+import { API_BASE_URL } from "./settings/api.js";
 
 renderMenu();
 formValidation();
+
+// -----------------------------------------------------------------------------
+
+const form = document.querySelector("form");
+const usernameInput = document.getElementById("registerUsername");
+const emailInput = document.getElementById("registerEmail");
+const passwordInput = document.getElementById("registerPassword");
+
+form.addEventListener("submit", handleRegistration);
+
+/**
+ * Handles the submission of a user registration form.
+ *
+ * This function prevents the default form submission behavior, collects
+ * user input values, sends a registration request to the server, and
+ * handles the response.
+ *
+ * @param {Event} event The event object representing the form submission.
+ * @returns {Promise<void>} A Promise that resolves when the registration is complete.
+ */
+async function handleRegistration(event) {
+  event.preventDefault();
+
+  const registerUrl = `${API_BASE_URL}/social/auth/register`;
+
+  const username = usernameInput.value;
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  if (username.length === 0 || email.length === 0 || password.length === 0) {
+    return;
+  } else {
+    const user = {
+      name: username,
+      email: email,
+      password: password,
+    };
+
+    try {
+      const postData = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      };
+
+      const response = await fetch(registerUrl, postData);
+      console.log(response);
+
+      if (response.ok) {
+        const json = await response.json();
+        console.log(json);
+        // Success message here
+
+        console.log("User registration was successful");
+      } else {
+        // Error message here
+
+        console.log(
+          "An error has occurred when attempting the user registration"
+        );
+      }
+    } catch (error) {
+      console.log(error);
+      // Error message here
+    }
+  }
+}
+
+// -----------------------------------------------------------------------------
