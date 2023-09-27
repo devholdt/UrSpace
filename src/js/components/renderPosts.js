c32bfc902001ce01fb1348c7fc81bf22252e3468
MHerholdt94 <mathiasher@live.no>
2023-09-27 17:22:16 +0200

    Post media update
    Better media display on posts, with just a thumbnail version of the media being shown on each post. Users can click the thumbnail to open a modal of the full sized image.

--- src/js/components/renderPosts.js
+++ src/js/components/renderPosts.js
@@ -20,32 +20,33 @@ export async function renderPosts(url) {
 
       for (const post of posts) {
         let postMedia;
+        let modalContent;
 
-        if (!post.media) {
-          postMedia = "";
+        if (post.media) {
+          const modalId = `modal-${post.id}`;
+          postMedia = `
+          <div class="thumbnail">
+            <img src="${post.media}" alt="Post media thumbnail" class="thumbnail-img" data-modal-id="${modalId}"
+          </div>`;
+          modalContent = `
+            <div class="modal" id="${modalId}">
+              <span class="close-btn" data-modal-id="${modalId}">&times;</span>
+                <img src="${post.media}" alt="Full-sized post media" class="modal-content">
+            </div>`;
         } else {
-          postMedia = `<img src="${post.media}" alt="Post media">`;
+          postMedia = "";
+          modalContent = "";
         }
 
         postsContainer.innerHTML += `
-          <div class="card m-4 rounded-0">
+          <div class="card m-4 rounded-0 post">
             <div class="card-header border-0 bg-white">
               <p>${post.title}</p>
             </div>
             <div class="card-body">
               <p class="card-text">${post.body}</p>
               ${postMedia}
-            </div>
-            <div class="btn-group m-3" role="group" aria-label="Post interaction">
-              <button class="btn btn-custom" title="Like">
-                <i class="fa-regular fa-thumbs-up"></i>
-              </button>
-              <button class="btn btn-custom" title="Repost">
-                <i class="fa-solid fa-retweet"></i>
-              </button>
-              <button class="btn btn-custom" title="Comment">
-                <i class="fa-regular fa-comment"></i>
-              </button>
+              ${modalContent}
             </div>
           </div>`;
       }
@@ -54,4 +55,38 @@ export async function renderPosts(url) {
     console.log(error);
     message("error", "An error occured with the API call");
   }
+
+  attachEventListeners();
+}
+
+function openModal(modalId) {
+  const modal = document.getElementById(modalId);
+  modal.classList.add("modal-active");
+  modal.addEventListener("click", closeModal);
+  modal.querySelector(".modal-content").addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
+}
+
+function closeModal(e) {
+  if (
+    e.target.classList.contains("modal") ||
+    e.target.classList.contains("close-btn")
+  ) {
+    const modals = document.querySelectorAll(".modal");
+    modals.forEach((modal) => {
+      modal.classList.remove("modal-active");
+    });
+  }
+}
+
+function attachEventListeners() {
+  const thumbnailImages = document.querySelectorAll(".thumbnail-img");
+
+  thumbnailImages.forEach((thumbnail) => {
+    thumbnail.addEventListener("click", () => {
+      const modalId = thumbnail.getAttribute("data-modal-id");
+      openModal(modalId);
+    });
+  });
 }
