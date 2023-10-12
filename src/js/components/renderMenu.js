9b9aee9a6ae03cc78794e8bfec474820b0da65db
MHerholdt94 <mathiasher@live.no>
2023-10-12 20:44:33 +0200

    Hamburger menu woop
    The navigation menu now appear as a hamburger menu instead on smaller screen sizes (998px and lower). User info (in the header) also appears inside that menu instead.
    
    Fixed "Profile" link not displaying active state properly.
    
    Changed width of modals to be more responsive.
    
    Removed unused code and image.

--- src/js/components/renderMenu.js
+++ src/js/components/renderMenu.js
@@ -1,5 +1,5 @@
 import { getUser } from "../utilities/storage.js";
-import { URLS } from "../settings/constants.js";
+import { URLS, DEFAULT_URLS } from "../settings/constants.js";
 import { handleLogout } from "../utilities/clickEvents.js";
 
 /**
@@ -9,8 +9,9 @@ export default function renderMenu() {
   // Get the current page pathname
   const { pathname } = document.location;
 
-  // Select the navigation element
-  const nav = document.querySelector("nav");
+  // Select the navigation elements
+  const nav = document.querySelector("nav.position-sticky");
+  const hamburgerNav = document.querySelector(".navbar-nav");
 
   // Get user data if available
   const userData = getUser();
@@ -18,9 +19,8 @@ export default function renderMenu() {
   // Define navigation links with their href, text, and icon
   const links = [
     { href: URLS.FEED, text: "Home", icon: "fa-solid fa-house" },
-    // { href: URLS.PROFILE, text: "Profile", icon: "fa-solid fa-user" },
     {
-      href: `${URLS.PROFILE}?name=${userData.name}`,
+      href: URLS.PROFILE,
       text: "Profile",
       icon: "fa-solid fa-user",
     },
@@ -28,32 +28,40 @@ export default function renderMenu() {
   ];
 
   // Select the user profile navigation element
-  const navProfile = document.querySelector(".user-info");
+  const headerNavProfile = document.querySelector(".user-info-header");
+  const hamburgerNavProfile = document.querySelector(".user-info-hamburger");
 
   // Check if the user is authenticated
   if (userData) {
-    // Display the user profile navigation
-    navProfile.style.display = "flex";
-
     // Set a default avatar URL if the user's avatar is null
     if (userData.avatar === null) {
-      userData.avatar =
-        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-avatarture-973460_1280.png";
+      userData.avatar = DEFAULT_URLS.AVATAR;
     }
 
     // Render the user profile information
-    navProfile.innerHTML = `
+    headerNavProfile.innerHTML = `
     <div>
-      <img src="${userData.avatar}" class="avatar" alt="${userData.name}'s profile picture">
+      <img src="${userData.avatar}" class="avatar" alt="${userData.name}'s profile picture" onerror='this.src="${DEFAULT_URLS.AVATAR}"'>
       <a href="${URLS.PROFILE}?name=${userData.name}">${userData.name}</a>
     </div>
     <div>
-      <button id="btnLogout" class="border-0">Log out</button>
+      <button id="btnLogoutHeader" class="border-0">Log out</button>
     </div>`;
 
-    // Add a click event listener to the logout button
-    const logoutButton = document.getElementById("btnLogout");
-    logoutButton.addEventListener("click", handleLogout);
+    hamburgerNavProfile.innerHTML = `
+    <div>
+      <img src="${userData.avatar}" class="avatar" alt="${userData.name}'s profile picture" onerror='this.src="${DEFAULT_URLS.AVATAR}"'>
+      <a href="${URLS.PROFILE}?name=${userData.name}">${userData.name}</a>
+    </div>
+    <div>
+      <button id="btnLogoutHamburger" class="btn btn-outline-dark">Log out</button>
+    </div>`;
+
+    // Add a click event listener to the logout buttons
+    const logoutButtonHeader = document.getElementById("btnLogoutHeader");
+    const logoutButtonHamburger = document.getElementById("btnLogoutHamburger");
+    logoutButtonHeader.addEventListener("click", handleLogout);
+    logoutButtonHamburger.addEventListener("click", handleLogout);
   }
 
   // Render the navigation links based on the current page
@@ -63,6 +71,19 @@ export default function renderMenu() {
       <a href="${link.href}" class="link-custom ${
         pathname === `/${link.href}` ? "active" : ""
       }"><i class="${link.icon}"></i> <span>${link.text}</span></a>`
+    )
+    .join("");
+
+  hamburgerNav.innerHTML = links
+    .map(
+      (link) => `
+      <li class="d-flex justify-content-center border-top py-2">
+        <a href="${link.href}" class="link-custom ${
+        pathname === `/${link.href}` ? "active" : ""
+      }">
+          <i class="${link.icon}"></i> <span>${link.text}</span>
+        </a>
+      </li>`
     )
     .join("");
 }
--- src/js/settings/constants.js
+++ src/js/settings/constants.js
@@ -17,6 +17,7 @@ export const URLS = {
   SEARCH: "searchResults.html",
 };
 
+// Define URLs for default images
 export const DEFAULT_URLS = {
   BANNER:
     "https://images.unsplash.com/photo-1557682260-96773eb01377?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2029&q=80",
