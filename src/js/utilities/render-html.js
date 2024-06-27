942b37b83fbf4aeaf42e8a82baa1815da0bf9fbe
MHerholdt94 <mathiasbherholdt@gmail.com>
2024-06-27 14:16:50 +0200

    creates templates of html files that is build with dynamically chosen stylesheet file path

---
+++ src/js/utilities/render-html.js
@@ -0,0 +1,24 @@
+const ejs = require("ejs");
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
+
+const renderFile = (templatePath, outputPath) => {
+	const template = fs.readFileSync(templatePath, "utf-8");
+	const renderedHtml = ejs.render(template, { env: process.env });
+
+	fs.writeFileSync(outputPath, renderedHtml, "utf-8");
+};
+
+renderFile(
+	path.join(__dirname, "../../../templates/index.ejs"),
+	path.join(__dirname, "../../../dist/index.html")
+);
+renderFile(
+	path.join(__dirname, "../../../templates/feed.ejs"),
+	path.join(__dirname, "../../../dist/feed.html")
+);
+renderFile(
+	path.join(__dirname, "../../../templates/profile.ejs"),
+	path.join(__dirname, "../../../dist/profile.html")
+);
